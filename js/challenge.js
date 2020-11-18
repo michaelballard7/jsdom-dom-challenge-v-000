995dcdb63ed2149ad1d62bdf56182d80e1cc6ced
Michael Ballard <ballard.michael9@gmail.com>
2020-11-18 20:14:52 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,164 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('Logging')
+
+    // create counterState = {num:0,like:0}
+    let counter = 0
+
+    const numbers = {};
+
+    // create likesState
+    let likesState = 0
+
+    // create pauseBtnState
+    let pauseBtnState = "running"
+
+    // select the counter display
+    let counterDisplay = document.querySelector('#counter');
+
+    // select incrementBtn
+    let incrementBtn = document.querySelector('#plus');
+
+    // select decrementBtn
+    let decrementBtn = document.querySelector('#minus');
+
+    // select the heartBtn
+    let heartBtn = document.querySelector('#heart');
+
+    // select like container
+    let likesContainer = document.querySelector('.likes')
+
+    // select the puase btn
+    let pauseBtn  = document.querySelector('#pause')
+
+    // select input form
+    let commentInput = document.querySelector('#comment-input');
+
+    // select comment container
+    let commentContainer = document.querySelector('#list')
+
+    // create  comment ulContainer and append to comments
+    let commentList = document.createElement('ul')
+
+
+    // set Timeout to check the counter and heart and update the likes
+    function heartBeatChecker(){
+
+            if (!numbers[counter]){
+                let counterState = {num:counter, likes: likesState}
+                numbers[counter] = counterState
+            } else {
+                numbers[counter].likes = likesState
+            }
+
+            // update number and count in li
+            let counterLikeDisplay = document.createElement('li');
+
+            let text =  document.createTextNode(`${numbers[counter].num} - likes: ${numbers[counter].likes}`);
+
+            counterLikeDisplay.appendChild(text);
+
+            likesContainer.appendChild( counterLikeDisplay);
+
+            likesState=0
+    }
+
+    // set interval to increment counter to increment by 1
+    function startCounter(){
+
+        setInterval( ()=> {
+            // increment counter  +1
+            counter++
+
+            // update the counter display
+            counterDisplay.innerText = counter
+
+            heartBeatChecker()
+
+        }, 1000)
+    }
+
+    // create comment
+    function createComment(){
+        // select inputValue on form submit
+
+            // create comment li
+
+            // append to container as adjancet html
+
+    }
+
+    function main(){
+
+        // start Counter
+        startCounter()
+
+        // begin heart like checker
+        let heartBeat = setTimeout( heartBeatChecker, 500)
+
+        // incrementBtn
+        incrementBtn.addEventListener('click', () => {
+            counter++;
+        })
+
+        // decrementBtn
+        decrementBtn.addEventListener('click', () => {
+            counter--;
+        })
+
+        // heartBtn
+        heartBtn.addEventListener('click',()=>{
+            likesState++
+            console.log(likesState)
+        })
+
+        // pauseBtn,
+        pauseBtn.addEventListener('click', () => {
+
+            console.log('pause is clicked')
+
+            // if state running
+            if(pauseBtnState === 'running') {
+                //  set to pause
+                pauseBtnState = "pause"
+                // clear interval to cancel set interval,
+                clearInterval(heartBeat)
+
+                // disable incrementBtn
+                incrementBtn.style.display="none"
+
+                // disable decrementBtn
+                decrementBtn.style.display="none"
+                // disable heartBtn
+                heartBtn.style.display="none"
+
+                // set pause Btn to resume
+                pauseBtnState = "resume"
+
+                return null
+            }
+
+            if(pauseBtnState){
+                console.log('fixed')
+            // if state paused
+                    // set to running
+                    // resetCounter state to zero
+                    // enable incrementBtn
+                    // enable decrementBtn
+                    // set pause btn to ❤️
+
+            }
+        })
+
+        // add event for comment Add
+
+
+    } // end of main
+
+
+
+
+
+    main()
+
+}) // end of load event
